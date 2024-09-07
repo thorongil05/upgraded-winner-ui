4e6585497eb03e085cdce2e083284b417176cf3b
thorongil05 <officialantonio@outlook.com>
2024-09-07 17:47:08 +0200

    feat: Add pipes

---
+++ src/app/features/starting/pipes/RolePipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Role } from '../player';
+@Pipe({
+  standalone: true,
+  name: 'role',
+})
+export class RolePipe implements PipeTransform {
+  transform(value: Role): string {
+    switch (value) {
+      case Role.GOALKEEPER:
+        return 'Portiere';
+      case Role.DEFENDER:
+        return 'Difensore';
+      case Role.MIDFIELDER:
+        return 'Centrocampista';
+      case Role.STRICKER:
+        return 'Attaccante';
+      default:
+        return '';
+    }
+  }
+}
---
+++ src/app/features/starting/pipes/TeamPipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Team } from '../player';
+@Pipe({
+  standalone: true,
+  name: 'team',
+})
+export class TeamPipe implements PipeTransform {
+  transform(value: Team): string {
+    switch (value) {
+      case Team.BOLOGNA:
+        return 'Bologna';
+      case Team.INTER:
+        return 'Inter';
+      default:
+        return '';
+    }
+  }
+}
