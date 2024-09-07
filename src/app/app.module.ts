143d3f0e7008148ed9d68c0ac0a35cb6ddc29376
thorongil05 <officialantonio@outlook.com>
2024-09-07 17:39:14 +0200

    feat: add pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerTableComponent } from './features/starting/player-table/player-table.component';
+import { RolePipe } from './features/starting/pipes/RolePipe';
+import { TeamPipe } from './features/starting/pipes/TeamPipe';
 
 @NgModule({
   declarations: [AppComponent, PlayerTableComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, RolePipe, TeamPipe],
   providers: [],
   bootstrap: [AppComponent],
 })
